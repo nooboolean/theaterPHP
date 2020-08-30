9ddda208449d1a379483a6fa579f7abdc315d6e6
nooboolean <nooboolean@i.softbank.jp>
2020-08-30 23:34:39 +0900

    CSRF対策実装

--- core/Controller.php
+++ core/Controller.php
@@ -74,4 +74,36 @@ abstract class Controller
         $this->response->setStatusCode(302, 'Found');
         $this->response->setHttpHeader('Location', $uri);
     }
+
+    protected function generateCsrfToken($formName)
+    {
+        $key = 'csrf_tokens/' . $formName;
+        $tokens = $this->session->get($key, array());
+        if (count($tokens) >= 10) {
+            array_merge($tokens);
+        }
+
+        $token = sha1($formName . session_id() . microtime());
+        $tokens[] = $token;
+
+        $this->session->set($key, $tokens);
+
+        return $token;
+    }
+
+    protected function checkCsrfToken($formName, $token)
+    {
+        $key = 'csrf_tokens/' . $formName;
+        $tokens = $this->session->get($key, array());
+
+        if (($tokenPointer = array_search($token, $tokens, true)) !== false) {
+            unset($tokens[$tokenPointer]);
+            $this->session->set($key, $tokens);
+
+            return true;
+        }
+        return false;
+    }
+
+    
 }
