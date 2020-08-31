b1136318659deaffe53d15ae5f900c36801ac1a0
nooboolean <nooboolean@i.softbank.jp>
2020-08-31 23:59:23 +0900

    Exeptionクラスを別フォルダで管理

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,6 @@ require 'core/ClassLoader.php';
 
 $loader = new ClassLoader();
 $loader->registerDirectory(dirname(__FILE__) . '/core');
-$loader->registerDirectory(dirname(__FILE__) . 'models');
+$loader->registerDirectory(dirname(__FILE__) . '/core/Eception');
+$loader->registerDirectory(dirname(__FILE__) . '/models');
 $loader->register();
